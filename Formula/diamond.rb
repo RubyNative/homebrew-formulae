bbe10a05b4f5e31596929ce5341e557b9901d835
Orta Therox <orta.therox@gmail.com>
2015-11-06 11:13:44 -0500

    Adds a formula for homebrew

---
+++ Formula/diamond.rb
@@ -0,0 +1,11 @@
+class Diamond < Formula
+  homepage 'https://github.com/RubyNative/Diamond'
+
+  # There are no release tags yet
+  head 'https://github.com/RubyNative/Diamond.git'
+
+  def install
+    system 'xcrun', "xcodebuild", 'build'
+    system 'ln ~/bin/diamond #{prefix}'
+  end
+end
